7eae1f62a7cac053ef1080721063a2c2de814170
Franck Binard <franckbinard@gmail.com>
2017-05-15 13:37:36 -0400

    Update Lesson2Program.cpp

--- Lesson2Program.cpp
+++ Lesson2Program.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
 
-
+void funFunctionAddress(int* argPtr){
+	std::cout	<< "In funFunctionAddress's world, "
+			<< "the address of arg is:" << argPtr
+			<< " and its value is:" << *argPtr << std::endl;
+}
 void funFunctionReference(int& arg){
 	std::cout	<< "In funFunctionReference's world, "
 			<< "the address of arg is:" << &arg
 			<< " and its value is:" << arg << std::endl;
 }
-
 void funFunctionValue(int arg){
 	std::cout	<< "In funFunctionValue's world, "
 			<<"the address of arg is:" << &arg 
 			<< " and its value is:" << arg << std::endl;
 }
-
 int main(){
 	int arg = 5;
 	std::cout	<< "In main's world, the address of arg is:"
 			<<&arg<<" and its value is:"
 			<<arg<<std::endl;
-	
-	/*calling funFunctionValue*/
+	//By Value
 	funFunctionValue(arg);
+	//By Reference
 	funFunctionReference(arg);
+	//By Address
+	funFunctionAddress(&arg);
 	return 1; 
 }
